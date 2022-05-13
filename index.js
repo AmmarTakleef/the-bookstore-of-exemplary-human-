04b81f76f77b455383c7bd5300ac75ae50f58c4f
AmmarTakleef <65197458+AmmarTakleef@users.noreply.github.com>
2022-05-13 14:59:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ReactDOM, {render} from "react-dom";
+import Style from "src/components/test/style.css";
+
+
+ReactDOM.render(<Style />, document.getElementById("root"));
+
+
